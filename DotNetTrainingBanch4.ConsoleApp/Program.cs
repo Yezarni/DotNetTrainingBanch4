f16716a63e7ca52e5f1c2b0396265feadcc5a48b
User <User@DESKTOP-G8G8Q7G>
2024-04-11 10:01:58 +0630

    Add project files.

---
+++ DotNetTrainingBanch4.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Data.SqlClient;
+
+Console.WriteLine("Hello, World!");
+SqlConnectionStringBuilder stringBuilder = new SqlConnectionStringBuilder();
+stringBuilder.DataSource = "USER";//sever name
+stringBuilder.InitialCatalog = "Dotnettrainingbatch4"; //database name
+stringBuilder.UserID = "sa";
+stringBuilder.Password = "sasa@123";
+SqlConnection connection = new SqlConnection(stringBuilder.ConnectionString);
+connection.Open();
+connection.Close();
+Console.ReadKey();
